b86bea7aeecaa9b1cb57e2eb344dc682ddc6396c
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-02-22 01:48:17 +0530

    Integrating app with backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_ALAN_KEY,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        }
+      },
+    });
+  }, []);
   return (
     <div>
       <h1>Alan AI News Application</h1>
